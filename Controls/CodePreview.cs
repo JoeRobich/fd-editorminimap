488d1c49f84d7604e3d080a653cfe116f34ab3f8
Joey Robichaud <joe.robich@gmail.com>
2015-05-30 17:46:32 -0400

    Check if line is whitespace before including its indentation when auto-scrolling

--- Controls/CodePreview.cs
+++ Controls/CodePreview.cs
@@ -133,10 +133,25 @@ namespace EditorMiniMap
             for (var offset = 0; offset < this.Editor.LinesOnScreen; offset++)
             {
                 var lineIndentation = this.Editor.GetLineIndentation(firstLine + offset);
-                column = Math.Min(column, this.Editor.GetLineIndentation(firstLine + offset));
+                if (lineIndentation == 0 && IsNullOrWhiteSpace(Editor.GetLine(firstLine + offset)))
+                    continue;
+
+                column = Math.Min(column, lineIndentation);
             }
 
             this.Editor.LineScroll(column - 2, 0);
+        }
+
+        static bool IsNullOrWhiteSpace(string value)
+        {
+            if (value == null) return true;
+
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (!char.IsWhiteSpace(value[i])) return false;
+            }
+
+            return true;
         }
     }
 }
